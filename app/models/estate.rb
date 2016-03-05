b6aa7ec7fa01e647e59506401e61bde16c20f251
softcomet <davesoft110@gmail.com>
2016-03-05 09:14:56 +0100

    strong parameter was defined

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -1,9 +1,3 @@
 class Estate < ActiveRecord::Base
-#   TYPES = %w( Movie )
-#   before_save :set_type
-#   validates :type, presense: true, :inclusion => {:in => TYPES}
 
-#   def set_type
-#     raiser "You must override this method in each model inheriting from Product!"
-#   end
 end
