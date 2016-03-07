8c005cb23c56145b5f9d523d4197ef0c1d1fa62a
softcomet <davesoft110@gmail.com>
2016-03-07 07:56:18 +0100

    csv column was defined

--- app/admin/estate.rb
+++ app/admin/estate.rb
@@ -1,9 +1,22 @@
 ActiveAdmin.register Estate do
 
-permit_params :loan_number, :product_description, :product_type, :census_tract, 
-              :income_level, :country, :state_fips, :purpose, :property_type, 
-              :fico, :application_date
+  permit_params :loan_number, :product_description, :product_type, :census_tract, 
+                :income_level, :country, :state_fips, :purpose, :property_type, 
+                :fico, :application_date
 
-active_admin_importable
+  active_admin_importable
 
+  csv do
+    column :loan_number
+    column :product_description
+    column :product_type
+    column :census_tract
+    column :income_level
+    column :country
+    column :state_fips
+    column :purpose
+    column :property_type
+    column :fico
+    column :application_date
+  end
 end
